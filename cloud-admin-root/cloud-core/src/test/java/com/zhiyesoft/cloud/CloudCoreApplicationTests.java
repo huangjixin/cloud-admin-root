0f7ac219206150cbf8ca3e27cf62334926a125c8
huangjixin <517714860@qq.com>
2018-12-24 10:22:40 +0800

    添加核心

---
+++ cloud-admin-root/cloud-core/src/test/java/com/zhiyesoft/cloud/CloudCoreApplicationTests.java
@@ -0,0 +1,12 @@
+package com.zhiyesoft.cloud;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class CloudCoreApplicationTests {
+
+//	@Test
+//	public void contextLoads() {
+//	}
+
+}
+
